1b0e83aa562521c450fac473542651c27468e1d6
Raphaël Mourtzanakis <raphael.mourtzanakis@lmdsio.fr>
2025-10-10 11:11:27 +0200

    Version 6

--- resources/views/listMangas.blade.php
+++ resources/views/listMangas.blade.php
@@ -2,42 +2,31 @@
 @section('content')
     <h1>Liste des Mangas</h1>
 
+    <table class="table table-bordered table-striped" style="margin-bottom: 50px;">
         <thead class="table table-bordered table-striped">
             <tr>
-                    <td>
-                        Couvertures
-                    </td>
-                    <td>
-                        Titres
-                    </td>
-                    <td>
-                        Prix
-                    </td>
-                    <td>
-                        Genres
-                    </td>
-                    <td>
-                        Nom des dessinateurs
-                    </td>
-                    <td>
-                        Nom des scénaristes
-                    </td>
+                <th>Couvertures</th>
+                <th>Titres</th>
+                <th>Prix</th>
+                <th>Genres</th>
+                <th>Nom des dessinateurs</th>
+                <th>Nom des scénaristes</th>
+                <th></th>
             </tr>
         </thead>
 
         <tbody class="table table-bordered table-striped">
+            @foreach ($mangas as $ligne)
             <tr>
-                <td>
-                    <img class="img-thumbnail" src="{{ url('') }}">
-                </td>
-                <td>
-                    <a href="{{url('/')}}"><i class="bi"></i></a>
-                </td>
-                <td>
-                    <a onclick="return confirm('Supprimer ce manga ?')"
-                       href="{{url('/')}}"><i class="bi"></i></a>
-                </td>
+                <td><img class="img-thumbnail" src="{{ url('assets/images/'.$ligne->couverture) }}"></td>
+                <td><a href="{{url('/')}}"><i class="bi"></i></a></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><a onclick="return confirm('Supprimer ce manga ?')" href="{{url('/')}}"> <i class="bi bi-trash"></i> </a></td>
             </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
